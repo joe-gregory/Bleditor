15fe1337779def38928c6219796224032102a3ff
joe-gregory <josephedward2nd@gmail.com>
2025-04-06 09:15:29 -0700

    Migradint logic from RenderDocument

--- Bleditor/Bleditor.cs
+++ Bleditor/Bleditor.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Bleditor.Rendering;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace Bleditor
 {
     public class Bleditor : BleditorBase<BleditorCommands>
     {
+        private static readonly IReadOnlyCollection<BleditorCommands> _commands = Enum.GetValues(typeof(BleditorCommands)).Cast<BleditorCommands>().ToList();
+        private readonly RenderInstructionListMap _renderInstructionListMap = [];
+        protected CursorPosition _cursorPosition; //LEFT OFF HERE. CONSIDERING GETTING RID OF CursorPosition and just have Selection. 
         /// <summary>
         /// Applied to root tag
         /// </summary>
@@ -16,11 +21,12 @@ namespace Bleditor
         public string Style { get; set; } = string.Empty;
         [Parameter]
         public string RootHtmlTag { get; set; } = "div";
+        [Inject]
+        private IJSRuntime JSRuntime { get; set; } = default!;
         /// <summary>
         /// In case there is more than once instance, it only gets calculated once. Readonly ensures it's only computed once. 
         /// </summary>
         public override IReadOnlyCollection<BleditorCommands> Commands => _commands;
-        private static readonly IReadOnlyCollection<BleditorCommands> _commands = Enum.GetValues(typeof(BleditorCommands)).Cast<BleditorCommands>().ToList();
         public override Task Execute(BleditorCommands command)
         {
             return Task.CompletedTask;
